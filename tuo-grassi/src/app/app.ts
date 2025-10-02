6717037cfcf3b7144daefeba588741bc2c13fd4e
ELFEDE104 <159022056+ELFEDE104@users.noreply.github.com>
2025-10-02 08:15:50 +0000

    punto6

--- tuo-grassi/src/app/app.ts
+++ tuo-grassi/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SecondComponent } from './second-component/second-component';
+import { ThirdComponent } from './third-component/third-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SecondComponent, ThirdComponent ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
